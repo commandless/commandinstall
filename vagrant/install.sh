73d24c14d91102d9e924b76f1ffc82ec60637fea
Ram on Agmon <ram-on.agmon2@microfocus.com>
2020-02-17 17:48:49 +0200

    wip

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -12,16 +12,17 @@ function DockerInstall {
 
 function BackendPreInstall {
 	GraalvmVersion=graalvm-ce-java8-linux-amd64-19.3.1
-	sudo apt-get -y install build-essential libz-dev zlib1g-dev axel
+	sudo apt-get -y install build-essential libz-dev zlib1g-dev axel unzip zip 
 	if [ ! -f "${GraalvmVersion}.tar.gz" ]; then
 		axel -n 10 -a -c "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-19.3.1/${GraalvmVersion}.tar.gz"
 		tar zxf ${GraalvmVersion}
+  fi
 	FolderName=$( find /root/ -type d -name "graalvm-ce-java8*" )
 	echo $FolderName
 	echo 'export PATH=${PATH}'":${FolderName}/bin" >> /root/.bashrc
+  export PATH=${PATH}":${FolderName}/bin"
 	source /root/.bashrc
   gu install native-image
-fi
 }
 
 
@@ -42,8 +43,39 @@ docker run -d --ulimit memlock=-1:-1 -it --rm=true --memory-swappiness=0 \
   -e POSTGRES_DB=cmdls_db -p 5432:5432 postgres:10.5
 }
 
+
+function FrontendPreInstall {
+#  apt -y install nodejs npm
+#  npm install -g npm@latest
+  curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+}
+
+function FrontendInstall {
+  cd /root/
+#  git clone git@github.com:commandless/commandless-frontend.git 
+#  cd commandless-frontend
+  wget 'https://github.com/commandless/commandless-frontend/archive/master.zip'
+  unzip master.zip
+#  npx degit sveltejs/template svelte-app
+  mv commandless-frontend-master commandless-frontend
+  cd commandless-frontend
+  cd svelte-app
+  npm install
+}
+
+
+function FrontendRun {
+    cd /root/commandless-fronted
+    npm run dev &
+  
+}
+
 BackendPreInstall
 BackendInstall
 DockerInstall
 PostgrRun
 BackendRun
+FrontendPreInstall
+FrontendInstall
+FrontendRun
